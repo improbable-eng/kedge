0caf60cd8b180abec9d714f7b82f0edeaf36086b
Bartek Plotka <bwplotka@gmail.com>
2017-09-04 14:21:08 +0100

    Fixed race on mutliple writers/readers warning.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- http/lbtransport/transport.go
+++ http/lbtransport/transport.go
@@ -63,16 +63,13 @@ func New(ctx context.Context, targetAddr string, parent http.RoundTripper, resol
 	return s, nil
 }
 
-var (
-	resolveRetryBackoff = &backoff.Backoff{
+func (s *tripper) run(ctx context.Context, resolver naming.Resolver, targetAddr string) {
+	resolveRetryBackoff := &backoff.Backoff{
 		Min:    50 * time.Millisecond,
 		Jitter: true,
 		Factor: 2,
 		Max:    2 * time.Second,
 	}
-)
-
-func (s *tripper) run(ctx context.Context, resolver naming.Resolver, targetAddr string) {
 	var lastNextError error
 
 	for ctx.Err() == nil {

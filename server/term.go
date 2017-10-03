f85dbfdcb71d64af90ad42c30786dbeee3071a93
Bartek Plotka <bwplotka@gmail.com>
2017-10-03 14:13:07 +0100

    Addeed graceful shutdown for seemless k8s rollouts.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

---
+++ server/term.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/mwitkow/kedge/lib/sharedflags"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	signalShutdownTimeout = sharedflags.Set.Duration(
+		"server_sigterm_timeout",
+		40*time.Second,
+		"Timeout for graceful Shutdown after catching a signal. "+
+			"If the timeout expires, another SIGTERM is sent that skips the Shutdown handler")
+)
+
+// waitForAny blocks until any of given signal is received, or context is done.
+// The SIGTERM is signaled after some time, delaying it for servers to have time to graceful shutdown.
+func waitForAny(ctx context.Context, signals ...os.Signal) {
+	ch := make(chan os.Signal, len(signals))
+	signal.Notify(ch, signals...)
+	defer signal.Stop(ch)
+
+	var gotSignal os.Signal
+	select {
+	case <-ctx.Done():
+		return
+	case gotSignal = <-ch:
+		logrus.Infof("Got %s signal", gotSignal.String())
+	}
+
+	// Make sure we finally do SIGTERM after given time.
+	go func() {
+		ch := make(chan os.Signal, len(signals))
+		signal.Notify(ch, signals...)
+		defer signal.Stop(ch)
+
+		select {
+		case <-ctx.Done():
+			return
+		case <-ch:
+			return
+		case <-time.After(*signalShutdownTimeout):
+		}
+
+		logrus.Infof("Graceful Shutdown after %s timed out, re-sending SIGTERM.", gotSignal.String())
+		syscall.Kill(syscall.Getpid(), syscall.SIGTERM)
+	}()
+}

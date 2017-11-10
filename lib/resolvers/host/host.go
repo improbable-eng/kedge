bf81e9f13c41bf9bd15f083d659b6501b8233e04
Bartek Plotka <bwplotka@gmail.com>
2017-11-10 14:20:54 +0000

    Add HostResolver to the config.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

---
+++ lib/resolvers/host/host.go
@@ -0,0 +1,51 @@
+package hostresolver
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/improbable-eng/go-srvlb/grpc"
+	"github.com/improbable-eng/go-srvlb/srv"
+	pb "github.com/improbable-eng/kedge/_protogen/kedge/config/common/resolvers"
+	"google.golang.org/grpc/naming"
+)
+
+type hostResolverFn func(host string) (addrs []string, err error)
+
+var (
+	ParentHostResolver hostResolverFn = net.LookupHost
+)
+
+func NewFromConfig(conf *pb.SrvResolver) (target string, namer naming.Resolver, err error) {
+	parent := ParentHostResolver
+	return conf.GetDnsName(), grpcsrvlb.New(newHostResolver(conf.PortOverride, parent)), nil
+}
+
+type hostResolver struct {
+	hostResolverFn hostResolverFn
+	port           uint32
+}
+
+// newHostResolver uses results from parent resolver and adds a port to it to implement srv.Resolver.
+func newHostResolver(port uint32, hostResolverFn hostResolverFn) srv.Resolver {
+	return &hostResolver{
+		hostResolverFn: hostResolverFn,
+		port:           port,
+	}
+}
+
+func (r *hostResolver) Lookup(domainName string) ([]*srv.Target, error) {
+	ips, err := r.hostResolverFn(domainName)
+	if err != nil {
+		return nil, err
+	}
+
+	var targets []*srv.Target
+	for _, ip := range ips {
+		targets = append(targets, &srv.Target{
+			DialAddr: net.JoinHostPort(ip, fmt.Sprintf("%d", r.port)),
+		})
+	}
+
+	return targets, nil
+}

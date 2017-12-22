929be204cbfea5bd404e9728708799ee7ecc14d2
Bartek Plotka <bwplotka@gmail.com>
2017-12-22 14:53:32 +0000

    Fixed function.
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- pkg/grpc/metadata/metadata.go
+++ pkg/grpc/metadata/metadata.go
@@ -9,7 +9,7 @@ import (
 func CloneIncomingToOutgoing(ctx context.Context) context.Context {
 	md, ok := metadata.FromIncomingContext(ctx)
 	if !ok {
-		md = metadata.Pairs()
+		return ctx
 	}
 	// Copy the inbound metadata explicitly.
 	return metadata.NewOutgoingContext(ctx, md.Copy())

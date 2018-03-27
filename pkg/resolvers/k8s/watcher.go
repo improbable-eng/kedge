c3030af57cc84a2b92fbd41c9b678962c2160d4f
Bartek Plotka <bwplotka@gmail.com>
2018-03-27 15:35:26 +0200

    kedge: Fixed critical issue. Added missing reconnect loop inside watcher. (#113)
    * kedge: Fixed critical issue. Added missing reconnect loop inside watcher.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * Resolver should not return error as well.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * Simplified resolver.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * Added stale stream support.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * Fixed test.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * Addressed comments.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * Fixed annoying thing.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * Addressed issue.
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>
    
    * tripper: Refactored http tripper resolver usage to match interface. (#114)
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- pkg/resolvers/k8s/watcher.go
+++ pkg/resolvers/k8s/watcher.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 	"net"
 
+	"io"
+
+	"time"
+
+	"github.com/improbable-eng/kedge/pkg/sharedflags"
+	"github.com/jpillora/backoff"
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/sirupsen/logrus"
@@ -13,22 +19,34 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 )
 
+var (
+	// TODO(bplotka): Check if that is really needed. I observed stale stream when you kill network interface that
+	// this connection is using. The HTTP Get request is then watching on response that will never happen.
+	// Ensure there is no better solution to fix this.
+	flagResyncTimeout = sharedflags.Set.Duration("k8sresolver_resync_timouet", 30*time.Minute,
+		"Time without updates after which stream is assumed stale.")
+	staleStreamError = errors.New("stream is stale. reconnecting")
+)
+
 // A Watcher provides name resolution updates by watching endpoints API.
 // It works by watching endpoint Watch API (retries if connection broke). Returned events with
 // changes inside endpoints are translated to resolution naming.Updates.
 type watcher struct {
 	logger logrus.FieldLogger
 
-	ctx    context.Context
-	cancel context.CancelFunc
-	target targetEntry
+	ctx      context.Context
+	cancel   context.CancelFunc
+	target   targetEntry
+	epClient endpointClient
 
-	streamer   *streamer
 	addrsState map[string]struct{}
 
 	resolvedAddrs     prometheus.Gauge
 	watcherErrs       prometheus.Counter
 	watcherGotChanges prometheus.Counter
+
+	streamer           *streamer
+	streamRetryBackoff *backoff.Backoff
 }
 
 func startNewWatcher(
@@ -39,31 +57,56 @@ func startNewWatcher(
 	watcherErrs prometheus.Counter,
 	watcherGotChanges prometheus.Counter,
 ) (*watcher, error) {
-	// NOTE(bplotka): Would love to have proper context from above but naming.Resolver does not allow that.
 	ctx, cancel := context.WithCancel(context.Background())
-
-	s, err := startNewStreamer(ctx, target, epClient)
-	if err != nil {
-		return nil, err
-	}
-
 	return &watcher{
-		logger:     logger,
-		ctx:        ctx,
-		cancel:     cancel,
-		target:     target,
-		streamer:   s,
-		addrsState: map[string]struct{}{},
-
+		logger:            logger,
+		ctx:               ctx,
+		cancel:            cancel,
+		target:            target,
+		epClient:          epClient,
+		addrsState:        map[string]struct{}{},
 		resolvedAddrs:     resolvedAddrs,
 		watcherErrs:       watcherErrs,
 		watcherGotChanges: watcherGotChanges,
+		streamer:          nil,
+		streamRetryBackoff: &backoff.Backoff{
+			Min:    50 * time.Millisecond,
+			Jitter: true,
+			Factor: 2,
+			Max:    2 * time.Second,
+		},
 	}, nil
 }
 
 // Close closes the watcher, cleaning up any open connections.
 func (w *watcher) Close() {
+	if w.streamer != nil {
+		w.streamer.Close()
+	}
+
 	w.cancel()
+}
+
+func (w *watcher) startNewStreamerWithRetry(ctx context.Context) *streamer {
+	if ctx.Err() != nil {
+		return nil
+	}
+
+	for {
+		s, err := startNewStreamer(w.target, w.epClient)
+		if err == nil {
+			w.streamRetryBackoff.Reset()
+			return s
+		}
+		s.Close()
+		w.logger.WithError(err).Warnf("k8sresolver: failed to start watching endpoint events for target %v", w.target)
+
+		select {
+		case <-time.After(w.streamRetryBackoff.Duration()):
+		case <-ctx.Done():
+			return nil
+		}
+	}
 }
 
 // Next updates the endpoints for the targetEntry being watched.
@@ -71,31 +114,46 @@ func (w *watcher) Close() {
 func (w *watcher) Next() ([]*naming.Update, error) {
 	if w.ctx.Err() != nil {
 		// We already stopped.
-		return []*naming.Update(nil), errors.Wrap(w.ctx.Err(), "k8sresolver: watcher.Next already stopped or Next returned error already. "+
+		return nil, errors.Wrap(w.ctx.Err(), "k8sresolver: watcher.Next already stopped or Next returned error already. "+
 			"Note that watcher errors are not recoverable.")
 	}
-	u, err := w.next()
-	if err != nil {
-		if w.ctx.Err() == nil {
-			// Only update those if watcher is not cancelled.
-			w.watcherErrs.Inc()
-			w.resolvedAddrs.Set(float64(0))
+
+	for {
+		if w.streamer == nil {
+			// Streamer closed / does not exists yet. Let's start it.
+			w.streamer = w.startNewStreamerWithRetry(w.ctx)
 		}
 
-		// Just in case.
-		w.Close()
-		return nil, errors.Wrap(err, "k8sresolver: ")
-	}
+		if w.ctx.Err() != nil {
+			return nil, w.ctx.Err()
+		}
 
-	w.resolvedAddrs.Set(float64(len(w.addrsState)))
-	return u, nil
+		u, err := w.next(w.ctx)
+		if err == nil {
+			// No error.
+			w.resolvedAddrs.Set(float64(len(w.addrsState)))
+			return u, nil
+		}
+
+		if err != staleStreamError && err != io.EOF {
+			// Errors worth to log.
+			w.logger.WithError(err).Warnf("k8sresolver: failed to watch endpoint events stream for target %v", w.target)
+			w.watcherErrs.Inc()
+		}
+
+		// Re-connect stream.
+		w.streamer.Close()
+		w.streamer = nil
+
+	}
+	return nil, w.ctx.Err()
 }
 
 // next gathers kube api endpoint watch changes and translates them to naming.Update set.
 // The main complexity is fact that naming.Update can be either Add or Delete. However kube events can be Add,
-// Delete or Modified. As a result we are required to maintain state. If the state is malformed we immediately return error
-// which will cause resync on caller side (new resolver).
-func (w *watcher) next() ([]*naming.Update, error) {
+// Delete or Modified. As a result we are required to maintain state.
+// If the state is malformed we immediately return error which will resync for our streamer.
+func (w *watcher) next(ctx context.Context) ([]*naming.Update, error) {
 	var (
 		updates         []*naming.Update
 		change          change
@@ -105,11 +163,16 @@ func (w *watcher) next() ([]*naming.Update, error) {
 
 	for len(updates) == 0 {
 		select {
-		case <-w.ctx.Done():
+		case <-ctx.Done():
 			// We already stopped.
-			return []*naming.Update(nil), w.ctx.Err()
+			return nil, ctx.Err()
+		case <-time.After(*flagResyncTimeout):
+			return nil, staleStreamError
 		case err := <-errCh:
-			return []*naming.Update(nil), errors.Wrap(err, "error on reading change stream")
+			if err == io.EOF {
+				return nil, io.EOF
+			}
+			return nil, errors.Wrap(err, "error on reading change stream")
 		case change = <-changeCh:
 		}
 		w.watcherGotChanges.Inc()
@@ -118,7 +181,7 @@ func (w *watcher) next() ([]*naming.Update, error) {
 			var err error
 			newAddrsState, err = subsetToAddresses(w.target, subset)
 			if err != nil {
-				return []*naming.Update(nil), errors.Wrap(err, "failed to convert k8s endpoint subset to update Addr")
+				return nil, errors.Wrap(err, "failed to convert k8s endpoint subset to update Addr")
 			}
 
 			if len(newAddrsState) > 0 {
@@ -133,16 +196,7 @@ func (w *watcher) next() ([]*naming.Update, error) {
 		// This way we can safely treat the object for Added and Modified as new state.
 		// Deleted event gives state before deletion, but we don't care. The whole object was deleted.
 		switch change.typ {
-		case watch.Added:
-			if len(w.addrsState) > 0 {
-				return []*naming.Update(nil), errors.Errorf("malformed internal state for addresses for target %v. "+
-					"We got added event type, but we already have some addresses from old updates: %v. Doing resync...", w.target, w.addrsState)
-			}
-
-			for addr := range newAddrsState {
-				updates = append(updates, w.addAddr(addr))
-			}
-		case watch.Modified:
+		case watch.Modified, watch.Added:
 			for addr := range newAddrsState {
 				_, ok := w.addrsState[addr]
 				if ok {
@@ -166,7 +220,7 @@ func (w *watcher) next() ([]*naming.Update, error) {
 			}
 		case watch.Deleted:
 			if len(w.addrsState) != len(newAddrsState) {
-				return []*naming.Update(nil), errors.Errorf("malformed internal state for addresses for target %v. "+
+				return nil, errors.Errorf("malformed internal state for addresses for target %v. "+
 					"We got delete event type with state before deletion and it does not match with that we tracked %v. "+
 					"State before deletion %v. Doing resync...", w.target, w.addrsState, newAddrsState)
 			}
@@ -174,7 +228,7 @@ func (w *watcher) next() ([]*naming.Update, error) {
 			for addr := range w.addrsState {
 				_, ok := newAddrsState[addr]
 				if !ok {
-					return []*naming.Update(nil), errors.Errorf("malformed internal state for addresses for target %v. "+
+					return nil, errors.Errorf("malformed internal state for addresses for target %v. "+
 						"We got delete event type with state before deletion and it does not match with that we tracked %v. "+
 						"State before deletion %v. Doing resync...", w.target, w.addrsState, newAddrsState)
 				}
@@ -182,7 +236,7 @@ func (w *watcher) next() ([]*naming.Update, error) {
 				updates = append(updates, w.delAddr(addr))
 			}
 		default:
-			return []*naming.Update(nil), errors.Errorf("unexpected change type %v", change.typ)
+			return nil, errors.Errorf("unexpected change type %v", change.typ)
 		}
 	}
 

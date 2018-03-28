880db27221dd64fca54d4db426cd7337cccec2c8
Stefan Sakalik <stefan@improbable.io>
2018-03-28 12:25:38 +0100

    use mustcompile

--- pkg/map/route.go
+++ pkg/map/route.go
@@ -11,26 +11,21 @@ type RouteGetter interface {
 	Route(hostPort string) (*Route, bool, error)
 }
 
+var ipMatchRegexp = regexp.MustCompile(`^\d+\.\d+\.\d+\.\d+$`)
+
 type routeMapper struct {
-	routes        []RouteGetter
-	ipMatchRegexp *regexp.Regexp
+	routes []RouteGetter
 }
 
 // RouteMapper is a mapper that resolves Route based on given DNS.
 func RouteMapper(r []RouteGetter) *routeMapper {
-	ipMatchRegexp, err := regexp.Compile(`^\d+\.\d+\.\d+\.\d+$`)
-	if err != nil {
-		// This should never panic in production, only in unit tests.
-		panic(fmt.Sprintf("failed to compile regular expression: %v", err))
-	}
 	return &routeMapper{
-		routes:        r,
-		ipMatchRegexp: ipMatchRegexp,
+		routes: r,
 	}
 }
 
 func (m *routeMapper) Map(targetDnsName string, targetPort string) (*Route, error) {
-	if m.ipMatchRegexp.MatchString(targetDnsName) {
+	if ipMatchRegexp.MatchString(targetDnsName) {
 		return nil, errors.Errorf("kedge requires hostname to proxy to but was given IP address %s:%s instead", targetDnsName, targetPort)
 	}
 
